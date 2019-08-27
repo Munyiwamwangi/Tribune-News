6af519a95df11509cad90cb9c59a1756d89cdac8
Munyiwamwangi <josseymunyi@gmail.com>
2019-08-27 20:46:25 +0300

    removed editor class to allow user to be the editor

--- news/models.py
+++ news/models.py
@@ -8,21 +8,20 @@ class NewsLetterRecipients(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField()
 
+# class Editor(models.Model):
+#     first_name = models.CharField(max_length = 30)
+#     last_name = models.CharField(max_length = 30)
+#     email = models.EmailField()
+#     phone_number = models.CharField(max_length = 10, blank = True)
 
-class Editor(models.Model):
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 30)
-    email = models.EmailField()
-    phone_number = models.CharField(max_length = 10, blank = True)
+#     def __str__(self):
+#         return self.first_name
 
-    def __str__(self):
-        return self.first_name
+#     def save_editor(self):
+#         self.save()
 
-    def save_editor(self):
-        self.save()
-
-    class Meta:
-        ordering = ['first_name']
+#     class Meta:
+#         ordering = ['first_name']
 
 class Tags(models.Model):
     name = models.CharField(max_length = 30)
