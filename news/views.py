d1600df6ecd774a92c5469f9121d867f801da389
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-03 17:24:00 +0300

    server changes

--- news/views.py
+++ news/views.py
@@ -1,84 +1,76 @@
-from django.contrib.auth.decorators import login_required
-from .email import send_welcome_email
-from .forms import NewArticleForm, NewsLetterForm
-from django.shortcuts import render,redirect
-from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
-from .models import Article,NewsLetterRecipients
-from .forms import forms
-
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .models import Article, NewsLetterRecipients
+from .forms import NewsLetterForm, NewArticleForm
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
+from .forms import NewArticleForm, NewsLetterForm
 # Create your views here.
-# def welcome(request):
-#     return render(request,"welcome.html")
+
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
 
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
     if request.method == 'POST':
         form = NewsLetterForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['your_name']
-            email = form.cleaned_data['email']
+        if request.method == 'POST':
+            form = NewsLetterForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['your_name']
+                email = form.cleaned_data['email']
+                recipient = NewsLetterRecipients(name=name, email=email)
+                recipient.save()
+                send_welcome_email(name, email)
 
-            recipient = NewsLetterRecipients(name = name,email =email)
-            recipient.save()
-            send_welcome_email(name,email)
-            
-            HttpResponseRedirect('news_today')
+                HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
-    return render(request, 'all-news/today-news.html', {"news":news,"date":date, 'letterForm':form})
 
-def convert_dates(dates):
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
+    return render(request, 'all-news/today-news.html', {"date": date, "news": news, "letterForm": form})
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
 
 def past_days_news(request, past_date):
-    #Converts data from the string to url
     try:
-
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
-         # Raise 404 error when ValueError is thrown
+        # Raise 404 error when ValueError is thrown
         raise Http404()
         assert False
 
-    day = convert_dates(date)
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
     news = Article.days_news(date)
-    return render(request, 'all-news/past-news.html', {"date": date,"news": news})
+    return render(request, 'all-news/past-news.html', {"date": date, "news": news})
+
 
 def search_results(request):
-    
+
     if 'article' in request.GET and request.GET["article"]:
         search_term = request.GET.get("article")
         searched_articles = Article.search_by_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+        return render(request, 'all-news/search.html', {"message": message, "articles": searched_articles})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-news/search.html',{"message":message})
+        return render(request, 'all-news/search.html', {"message": message})
 
 
 @login_required(login_url='/accounts/login/')
 def article(request, article_id):
     try:
-        article = Article.objects.get(id = article_id)
+        article = Article.objects.get(id=article_id)
     except DoesNotExist:
         raise Http404()
-    return render(request, "all-news/article.html",{"article":article})
+    return render(request, "all-news/article.html", {"article": article})
 
 
 @login_required(login_url='/accounts/login/')
@@ -94,4 +86,4 @@ def new_article(request):
 
     else:
         form = NewArticleForm()
-    return render(request, 'new-article.html', {"form": form})
+    return render(request, 'new_article.html', {"form": form})
