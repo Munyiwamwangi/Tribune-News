42b1f2ece09505ed54fa1140d2d16cf8e9ce3605
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-04 20:03:38 +0300

    aadding data to database using postman, API configuration successful and running well in the postman, however, its not clear what the API that i can share is, will learn tommorrow

--- news/models.py
+++ news/models.py
@@ -64,7 +64,9 @@ class Article(models.Model):
         return news
 
 #RESTFUL APIs
+
 class MoringaMerch(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20)
+    
